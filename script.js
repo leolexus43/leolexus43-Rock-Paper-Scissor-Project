a103293c4509bd5818a09afc03fbb0e8aa56a1b2
leolexus43 <danielbilongo2008@gmail.com>
2026-02-04 20:12:13 +0100

    This is a visual update for my rock paper scissor web game, this update has been created in a new branch

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+// Elements
+
+const para = document.querySelector("#resultDisplay")
+const scoreDisplay = document.querySelector(".score")
+const matchPointDisplayer = document.querySelector("#matchPointDisplayer")
+
+const ScissorButton = document.querySelector(".scissor")
+const rockButton = document.querySelector(".rock")
+const paperButton = document.querySelector(".paper")
+
+
+// Numbers
+
+let humanScore = 0;
+let computerScore  = 0;
+const maxPoint = 5;
+
+// You idiot this is literally such a dumb simple idea how come you not think about this.
+function resetGamePoints() {
+   humanScore = 0;
+   computerScore = 0;
+} 
+
+
+
+
+// The following function gets the computer choice (its random obviously smh)
 function getComputerChoice () {
     const num = Math.floor(Math.random() * 3)
     let choice = "";
@@ -21,51 +48,116 @@ function getComputerChoice () {
     return choice
 };
 
+let humanChoice = "";
 
-function getHumanChoice() {
 
-    let message = window.prompt("Choose : Rock , Paper or Scissor ?")
+// The following functions are temporary as they might not be the most efficient (not sure)
 
-    return message
-}
-let humanChoice = getHumanChoice();
-let computerChoice = getComputerChoice();
-
-let humanScore = 0;
-let computerScore  = 0;
-
-/*function incrementHumanScore(num) {
-  return num + 1;
+function choseRock() {
+   humanChoice = "Rock"
+   console.log("You chose Rock!")
 }
 
-function incrementClankaScore(num) {
-  return num + 1;
-}*/
+function chosePaper() {
+  humanChoice = "Paper"
+  console.log("You chose Paper!")
+}
+
+function choseScissor() {
+    humanChoice = "Scissor"
+    console.log("You chose scissor!")
+}
 
 
-function playGame(humChoice, clankaChoice) {
+
+function playGame() {
+    let computerChoice = getComputerChoice();
+    console.log(humanScore, computerScore)
+
+
+    //Win conditions
+if ((humanChoice === "Rock") && computerChoice === "Scissor" || (humanChoice === "Scissor") && computerChoice === "Paper" || (humanChoice === "Paper") && computerChoice === "Rock") {
+        humanScore += 1;
+
+        if (humanScore === maxPoint) {
+            console.log("Max points Reached!")
+
+
+            matchPointDisplayer.textContent = "Max points Reached! Match ended . You won!"
+
+            setTimeout(() => {
+                resetGamePoints()
+
+                scoreDisplay.textContent = `Score : You (${humanScore}) & Bot (${computerScore})`
+           
+                 matchPointDisplayer.textContent = "New match starting soon..." 
+                 para.textContent = ""
+
+                console.log("Points removed!")
+            }, 2000)
+
+        
+              setInterval(()=> {
+
+                       console.log('This is a SetInterval method')
+                    }, 3000)
+            
+        }
+
+   console.log("You win " + humanChoice + " beats " + computerChoice + " !")
+   para.textContent = "You win, " + humanChoice + " beats " + computerChoice + " !"
+   scoreDisplay.textContent = `Score : You (${humanScore}) & Bot (${computerScore})`
    
-if ((humChoice === "rock" || humChoice === "Rock") && clankaChoice === "Scissor" || (humChoice === "Scissor" || humChoice === "scissor") && clankaChoice === "Paper" || (humChoice === "Paper" || humChoice === "paper") && clankaChoice === "Rock") {
-        humanScore = Number(humanScore) + 1;
 
-   console.log("You win " + humChoice + " beats " + clankaChoice + " !")
-   
-
-  console.log("C" + computerScore + ":H" + humanScore)
+  console.log(`[${computerScore}]` + " : " + `[${humanScore}]` )
        
-    } else if (clankaChoice === "Rock" && humChoice === "Scissor" || clankaChoice === "Scissor" && humChoice === "Paper" || clankaChoice === "paper" && humChoice === "Rock") {
-          
+    //
+    } else if (computerChoice === "Rock" && humanChoice === "Scissor" || computerChoice === "Scissor" && humanChoice === "Paper" || computerChoice === "Paper" && humanChoice === "Rock") {
+            para.textContent = "You Lost, " + computerChoice + " beats " + humanChoice + " !"
+          scoreDisplay.textContent = `Score : You (${humanScore}) & Bot (${computerScore})`
 
-         computerScore = Number(computerScore) + 1;
+         computerScore = computerScore + 1;
 
-      console.log("You lost ! " + clankaChoice + " beats " + humChoice + " !")
-        console.log("Your choice : " + humChoice)
-        console.log("C" + computerScore + ":H" + humanScore)
 
-         } else if (humChoice === clankaChoice || humChoice === clankaChoice) {
+          if (computerScore === maxPoint) {
+            console.log("Max points Reached!")
+
+                     
+                    setInterval(()=> {
+
+                       console.log('This is a SetInterval method')
+                    }, 2000)
+
+            matchPointDisplayer.textContent = "Max points Reached! Match ended . You Loose!"
+            scoreDisplay.textContent = `Score : You (${humanScore}) & Bot (${computerScore})`
+            
+
+            setTimeout(() => {
+                 resetGamePoints()
+
+             scoreDisplay.textContent = `Score : You (${0}) & Bot (${0})`
+             matchPointDisplayer.textContent = ""
+             para.textContent = ""
+
+                console.log("Points removed!")
+            }, 2000)
+
+            setTimeout(() => {
+                matchPointDisplayer.textContent = "..."
+            }, 2000)
+        }
+
+      console.log("You lost ! " + computerChoice + " beats " + humanChoice + " !")
+         
+        console.log("Your choice : " + humanChoice)
+        console.log(`[${computerScore}]` + " : " + `[${humanScore}]` )
+
+         } else if (humanChoice === computerChoice) {
+               scoreDisplay.textContent = `Score : You (${humanScore}) & Bot (${computerScore})`
+               para.textContent = "Tie !"
              console.log("Tie!")
-             computerScore = Number(computerScore) + 1;
              };
+
+             scoreDisplay.textContent = `Score : You (${humanScore}) & Bot (${computerScore})`
      
 }
-playGame(humanChoice, computerChoice)
